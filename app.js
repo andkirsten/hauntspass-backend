521a69a221ca007381613238f6338f30b9aedb02
andkirsten <and.kirsten@gmail.com>
2023-09-05 13:40:52 +0900

    setting up database

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/haunts", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  // eslint-disable-next-line no-console
+  .then(() => console.log("Connected to MongoDB"))
+  // eslint-disable-next-line no-console
+  .catch((err) => console.log(err));
 
 const { PORT = 3001 } = process.env;
 
@@ -9,5 +22,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: [8, "Password must be at least 8 characters long"],
+    select: false,
+  },
+});
