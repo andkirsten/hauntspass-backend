147ab775a372b5af418dd1de8c4e3be0af9fb3c2
andkirsten <and.kirsten@gmail.com>
2023-09-05 13:24:28 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const { PORT = 3001 } = process.env;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
