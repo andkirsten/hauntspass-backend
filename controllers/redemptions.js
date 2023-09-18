0c6386610e0149f8d7bc10b47704f3330d83fbcf
andkirsten <and.kirsten@gmail.com>
2023-09-18 13:59:25 +0900

    adjustment to redemption

--- controllers/redemptions.js
+++ controllers/redemptions.js
@@ -3,15 +3,14 @@ const ConflictError = require("../utils/errors/ConflictError");
 const BadRequestError = require("../utils/errors/BadRequestError");
 
 exports.redeemReward = (req, res, next) => {
-  const { redemptionId } = req.body;
+  const { rewardId, pass } = req.body;
   Redemption.create({
-    redemptionId,
-    rewardId: req.reward._id,
-    pass: req.pass._id,
+    rewardId,
+    pass,
   })
     .then((redemption) => {
       res.send({
-        redemptionId,
+        _id: redemption._id,
         rewardId: redemption.rewardId,
         pass: redemption.pass,
       });
@@ -20,7 +19,7 @@ exports.redeemReward = (req, res, next) => {
       if (err.name === "MongoError" && err.code === 11000) {
         next(
           new ConflictError(
-            "This Redemption ID has already been used to redeem a reward",
+            "This pass has already been used to redeem this reward",
           ),
         );
       }
