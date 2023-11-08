4e26bdc21e38bee3be3140745220a5b5b941ef63
andkirsten <and.kirsten@gmail.com>
2023-11-08 11:50:44 +0900

    updating comments, readme updated, moved routes to index.js, rate-limiter added, added mongo server address to config

--- controllers/rewards.js
+++ controllers/rewards.js
@@ -54,65 +54,65 @@ exports.createReward = (req, res, next) => {
     });
 };
 
-// exports.updateReward = (req, res, next) => {
-//   const { rewardId } = req.params;
-//   const {
-//     rewardTitle,
-//     rewardLocation,
-//     rewardDescription,
-//     rewardImageUrl,
-//     rewardExtras,
-//     rewardTerms,
-//   } = req.body;
-//   Reward.findOneAndUpdate(
-//     { rewardId },
-//     {
-//       rewardTitle,
-//       rewardLocation,
-//       rewardDescription,
-//       rewardImageUrl,
-//       rewardExtras,
-//       rewardTerms,
-//     },
-//     { new: true },
-//   )
-//     .then((reward) => {
-//       res.send({
-//         _id: reward._id,
-//         eventId: reward.eventId,
-//         rewardTitle: reward.rewardTitle,
-//         rewardLocation: reward.rewardLocation,
-//         rewardDescription: reward.rewardDescription,
-//         rewardImageUrl: reward.rewardImageUrl,
-//         rewardExtras: reward.rewardExtras,
-//         rewardTerms: reward.rewardTerms,
-//       });
-//     })
-//     .catch((err) => {
-//       if (err.name === "MongoError" && err.code === 11000) {
-//         throw new ConflictError("This reward already exists");
-//       }
-//       if (err.name === "ValidationError") {
-//         throw new BadRequestError("Not Valid Reward ID");
-//       }
-//       next(err);
-//     });
-// };
+exports.updateReward = (req, res, next) => {
+  const { rewardId } = req.params;
+  const {
+    rewardTitle,
+    rewardLocation,
+    rewardDescription,
+    rewardImageUrl,
+    rewardExtras,
+    rewardTerms,
+  } = req.body;
+  Reward.findOneAndUpdate(
+    { rewardId },
+    {
+      rewardTitle,
+      rewardLocation,
+      rewardDescription,
+      rewardImageUrl,
+      rewardExtras,
+      rewardTerms,
+    },
+    { new: true },
+  )
+    .then((reward) => {
+      res.send({
+        _id: reward._id,
+        eventId: reward.eventId,
+        rewardTitle: reward.rewardTitle,
+        rewardLocation: reward.rewardLocation,
+        rewardDescription: reward.rewardDescription,
+        rewardImageUrl: reward.rewardImageUrl,
+        rewardExtras: reward.rewardExtras,
+        rewardTerms: reward.rewardTerms,
+      });
+    })
+    .catch((err) => {
+      if (err.name === "MongoError" && err.code === 11000) {
+        throw new ConflictError("This reward already exists");
+      }
+      if (err.name === "ValidationError") {
+        throw new BadRequestError("Not Valid Reward ID");
+      }
+      next(err);
+    });
+};
 
-// exports.deleteReward = (req, res, next) => {
-//   const { rewardId } = req.params;
-//   Reward.findByIdAndDelete({ rewardId })
-//     .orFail(() => {
-//       next(new BadRequestError("Not Valid Reward ID"));
-//     })
-//     .then(() => res.send({ message: "Reward Deleted" }))
-//     .catch((err) => {
-//       if (err.name === "MongoError" && err.code === 11000) {
-//         next(new ConflictError("This reward already exists"));
-//       }
-//       if (err.name === "ValidationError") {
-//         next(new BadRequestError("Not Valid Reward ID"));
-//       }
-//       next(err);
-//     });
-// };
+exports.deleteReward = (req, res, next) => {
+  const { rewardId } = req.params;
+  Reward.findByIdAndDelete({ rewardId })
+    .orFail(() => {
+      next(new BadRequestError("Not Valid Reward ID"));
+    })
+    .then(() => res.send({ message: "Reward Deleted" }))
+    .catch((err) => {
+      if (err.name === "MongoError" && err.code === 11000) {
+        next(new ConflictError("This reward already exists"));
+      }
+      if (err.name === "ValidationError") {
+        next(new BadRequestError("Not Valid Reward ID"));
+      }
+      next(err);
+    });
+};
--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,14 +1,39 @@
 const winston = require("winston");
 const expressWinston = require("express-winston");
 
+const messageFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.printf(
+    ({ level, message, meta, timestamp }) =>
+      `${timestamp} ${level}: ${meta.error?.stack || message}`,
+  ),
+);
+
 const requestLogger = expressWinston.logger({
-  transports: [new winston.transports.File({ filename: "request.log" })],
-  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: messageFormat,
+    }),
+    new winston.transports.File({
+      filename: "request.log",
+      format: winston.format.json(),
+    }),
+  ],
 });
 
 const errorLogger = expressWinston.errorLogger({
-  transports: [new winston.transports.File({ filename: "error.log" })],
-  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: messageFormat,
+    }),
+    new winston.transports.File({
+      filename: "error.log",
+      format: winston.format.json(),
+    }),
+  ],
 });
 
-module.exports = { requestLogger, errorLogger };
+module.exports = {
+  requestLogger,
+  errorLogger,
+};
